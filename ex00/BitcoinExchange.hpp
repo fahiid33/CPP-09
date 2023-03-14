2c10a53d1fcf4e112418f4db3aacb3dc4757b7bb
fahiid33 <trey34809@gmail.com>
2023-03-14 22:28:13 +0100

    ex00 ..>

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fstitou <fstitou@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 22:12:00 by fstitou           #+#    #+#             */
+/*   Updated: 2023/03/14 22:14:58 by fstitou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOIN_EXCHANGE_HPP
+# define BITCOIN_EXCHANGE_HPP
+
+#include <fstream>
+#include <string>
+#include <iostream>
+
+class BitcoinExchange
+{
+	public :
+	BitcoinExchange();
+	~BitcoinExchange();
+	BitcoinExchange()
+};
+
+#endif
